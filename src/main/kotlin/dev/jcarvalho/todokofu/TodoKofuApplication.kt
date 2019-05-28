eda7c89ef02422597fd9c9a93da5341a4113e025
João Carvalho <joaopecarvalho@gmail.com>
2019-05-28 21:35:29 +0100

    Model and dummy endpoint

--- src/main/kotlin/dev/jcarvalho/todokofu/TodoKofuApplication.kt
+++ src/main/kotlin/dev/jcarvalho/todokofu/TodoKofuApplication.kt
@@ -3,17 +3,27 @@ package dev.jcarvalho.todokofu
 import org.springframework.boot.WebApplicationType
 import org.springframework.fu.kofu.application
 import org.springframework.fu.kofu.webflux.webFlux
+import org.springframework.web.reactive.function.server.body
+import reactor.core.publisher.Flux
 
 val app = application(WebApplicationType.REACTIVE) {
 	webFlux {
 		router {
-
+			GET("/todo") {
+				ok().body(Flux.just(TodoItem(id = "my-item", title = "Buy Milk")))
+			}
 		}
 		codecs {
 			jackson()
 		}
 	}
 }
+
+data class TodoItem(
+		val id: String,
+		val title: String,
+		val completed: Boolean = false
+)
 
 fun main(args: Array<String>) {
 	app.run(args)
