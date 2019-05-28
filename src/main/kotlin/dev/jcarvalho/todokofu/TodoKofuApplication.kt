40592fee9a89fa2b96316db9b7c3fd9a0b40b3c9
João Carvalho <joaopecarvalho@gmail.com>
2019-05-28 21:41:08 +0100

    MongoDB

--- src/main/kotlin/dev/jcarvalho/todokofu/TodoKofuApplication.kt
+++ src/main/kotlin/dev/jcarvalho/todokofu/TodoKofuApplication.kt
@@ -1,22 +1,35 @@
 package dev.jcarvalho.todokofu
 
 import org.springframework.boot.WebApplicationType
+import org.springframework.data.mongodb.core.ReactiveMongoOperations
+import org.springframework.data.mongodb.core.findAll
 import org.springframework.fu.kofu.application
+import org.springframework.fu.kofu.mongo.reactiveMongodb
 import org.springframework.fu.kofu.webflux.webFlux
 import org.springframework.web.reactive.function.server.body
-import reactor.core.publisher.Flux
 
 val app = application(WebApplicationType.REACTIVE) {
 	webFlux {
 		router {
+			val repository = ref<TodoItemRepository>()
 			GET("/todo") {
-				ok().body(Flux.just(TodoItem(id = "my-item", title = "Buy Milk")))
+				ok().body(repository.findAll())
 			}
 		}
 		codecs {
 			jackson()
 		}
 	}
+	reactiveMongodb()
+	beans {
+	    bean<TodoItemRepository>()
+	}
+}
+
+class TodoItemRepository(private val mongo: ReactiveMongoOperations) {
+
+	fun findAll() = mongo.findAll<TodoItem>()
+
 }
 
 data class TodoItem(
